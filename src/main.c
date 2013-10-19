57460d6857d04bbc76a608fe1cdb60ec17949aed
Carl Bauer <heycosmo@gmail.com>
2013-10-19 02:47:16 -0700

    Serving elevation to a browser! yip

--- src/main.c
+++ src/main.c
@@ -1,66 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "parse_request.h"
 #include "../libwebsockets/lib/libwebsockets.h"
-#include "../jsmn/jsmn.h"
 #include "../gridfloat/src/gridfloat.h"
+#include "../gridfloat/src/linear.h"
 
 
 #define JSMN_TOKEN_COUNT 128
 
 #define MAX_GRIDFLOAT_OPEN 10
+#define MAX_CHUNK_SIZE (128*128)
+#define SEND_HEADER_SIZE 16
 
-const char *bounds_formats[] = {
-    "lrbt",
-    "xyab",
-    "nwab"
-};
-
-struct msg_attr {
-    const char *name;
-    jsmntype_t type;
-    int parent;
-};
-
-
-
-
-
-enum {
-    MSG_ATTR_GRID,
-    MSG_ATTR_BOUNDS
-};
-
-const struct msg_attr msg_schema[] = {
-    { "grid", JSMN_OBJECT, 0, parse_grid,  },
-    { "bounds", JSMN_OBJECT, MSG_ATTR_GRID },
-    { "format", JSMN_STRING, MSG_ATTR_BOUNDS },
-    { "values", JSMN_ARRAY, MSG_ATTR_BOUNDS },
-    { "resolution", JSMN_ARRAY, MSG_ATTR_GRID },
-    { NULL, -1, -1 }
-};
-
-//static int
-//match_msg_attr(jsmntok_t *token) {
-//    struct msg_attr *attr;
-//    for (attr = msg_schema; attr->name != NULL; attr++) {
-//        
-//
-//    }
-//
-//}
-
-
-
-static char parse_err_msg[256];
-
-
-
-
+/* Change these to pull data from a different file. */
+const char *hdr = "/home/bauerca/.usgs/n3f/n46w122/floatn46w122_13.hdr";
+const char *flt = "/home/bauerca/.usgs/n3f/n46w122/floatn46w122_13.flt";
+static gf_struct gf;
 
 struct per_session_data__super_dumb {
-    int number;
+    gf_grid grid;
+    int err;
 };
+
+typedef enum {
+    SUCCESS = 0,
+    ERR_BIG_REQUEST = 1
+} err_t;
+
+const char *err_msgs[] = {
+    "",
+    "ERR: BIG"
+};
+
+static int
+write_header(struct libwebsocket *wsi, char *buf, const char *hdrmsg) {
+    int n, m;
+
+    n = sprintf(buf, "%*s", SEND_HEADER_SIZE, hdrmsg);
+    m = libwebsocket_write(wsi, buf, n, LWS_WRITE_TEXT);
+
+    printf("header: write %d bytes. wrote %d bytes.\n", n, m);
+    
+    if (m < n) {
+    	lwsl_err("ERROR %d writing header to di socket\n", n);
+    	return -1;
+    }
+
+    return 0;
+}
+
 
 static int
 callback_super_dumb(struct libwebsocket_context *context,
@@ -69,14 +58,18 @@ callback_super_dumb(struct libwebsocket_context *context,
     void *user, void *in, size_t len)
 {
     int n, m;
-    gf_grid grid;
-	unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + 512 +
+	unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING +
+                          SEND_HEADER_SIZE +
+                          sizeof(float) * MAX_CHUNK_SIZE +
 						  LWS_SEND_BUFFER_POST_PADDING];
-	unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
+	unsigned char *hdrp = &buf[LWS_SEND_BUFFER_PRE_PADDING];
+	unsigned char *datp = hdrp + SEND_HEADER_SIZE;
     struct per_session_data__super_dumb *pss =
         (struct per_session_data__super_dumb *)user;
+    gf_grid *grid = &pss->grid;
 
-    printf("rcved msg of length %d\n", len);
+    printf("\nrcved msg of length %d\n", len);
+    printf("reason: %d\n", reason);
     
     switch(reason) {
 	case LWS_CALLBACK_ESTABLISHED:
@@ -84,16 +77,43 @@ callback_super_dumb(struct libwebsocket_context *context,
 						 "LWS_CALLBACK_ESTABLISHED\n");
 		break;
     case LWS_CALLBACK_RECEIVE:
-        msg_to_grid((const char *)in, &grid);
+        pss->err = 0;
+
+        parse_request((const char *)in, grid);
+
+        printf("%d\n", MAX_CHUNK_SIZE);
+        if (grid->nx * grid->ny > MAX_CHUNK_SIZE)
+            pss->err = -1;
+
         libwebsocket_callback_on_writable(context, wsi);
         break;
 	case LWS_CALLBACK_SERVER_WRITEABLE:
-		n = sprintf((char *)p, "%d", pss->number++);
-		m = libwebsocket_write(wsi, p, n, LWS_WRITE_TEXT);
+        if (pss->err != 0) {
+            n = sprintf((char *)hdrp, "%*s", SEND_HEADER_SIZE, "ERR BIG");
+		    m = libwebsocket_write(wsi, hdrp, n, LWS_WRITE_TEXT);
+		    if (m < n) {
+		    	lwsl_err("ERROR %d writing data to di socket\n", n);
+		    	return -1;
+		    }
+            break;
+        }
+            
+        /* Write success header */
+        sprintf((char *)hdrp, "%*s", SEND_HEADER_SIZE, "SUCCESS");
+
+        /* Write data */
+        memset((void *)datp, 0, sizeof(float) * grid->nx * grid->ny);
+        gf_bilinear_interpolate(&gf, grid, (void *)datp);
+
+        /* Bytes we should write. */
+        n = SEND_HEADER_SIZE + sizeof(float) * grid->nx * grid->ny;
+		m = libwebsocket_write(wsi, hdrp, n, LWS_WRITE_BINARY);
+        printf("data: write %d bytes. wrote %d bytes.\n", n, m);
 		if (m < n) {
-			lwsl_err("ERROR %d writing to di socket\n", n);
+			lwsl_err("ERROR %d writing data to di socket\n", n);
 			return -1;
 		}
+        usleep(1);
 		break;
     default:
         break;
@@ -116,6 +136,8 @@ int main(int argc, char **argv) {
     struct lws_context_creation_info info;
     int n;
 
+    gf_open(hdr, flt, &gf);
+
     memset(&info, 0, sizeof info);
     info.port = 7631;
     info.iface = NULL;
@@ -135,5 +157,6 @@ int main(int argc, char **argv) {
     }
 
     libwebsocket_context_destroy(context);
+    gf_close(&gf);
     exit(0);
 }
--- src/parse_request.c
+++ src/parse_request.c
@@ -2,131 +2,23 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "../jsnn/jsnn.h"
 
+#define JSNN_TOKEN_COUNT 32
 
-
-/*
- * Typedef for a function that is used to parse a token
- * found by the jsnn parser.
- */
-typedef int (attr_parser)(const char *json, jsnntok_t *token,
-    void *user);
-
-
-struct attr_parser_info {
-    const char **path;
-    jsnntype_t type;
-    attr_parser *parser;
-};
-
-
-
-/*
- * grid.bounds.format
- * ------------------
- * "lrbt", "xywh", or "nwwh"
- */
-static const char *grid_bounds_format_path[4] = {
-    "grid",
-    "bounds",
-    "format",
-    NULL
-};
-
-static int
-attr_parser__grid_bounds_format(const char *json, jsnntok_t *token,
-    void *user)
-{
-    struct request *r = (struct request *)user;
-    strncpy(r->grid.bounds.format, json + token->start,
-        token->end - token->start);
-}
-
-static struct attr_parser_info grid_bounds_format =
-{
-    grid_bounds_format_path,
-    JSNN_STRING,
-    attr_parser__grid_bounds_format
-};
-
-
-
-/*
- * grid.bounds.values
- * ------------------
- * A length-4 array of doubles representing the bounding box.
- * Interpretation of array of values depends on grid.bounds.format.
- */
-static int
-put_grid_bounds_value(size_t index, const char *value, void *user)
-{
-    struct request *r = (struct request *)user;
-    r->grid.bounds.values[index] = atof(value);
-    return 0;
-}
-
-static struct attr_array grid_bounds_values = {
-    { "values", "bounds", "grid", NULL },
-    JSNN_ARRAY,
-    4,
-    put_grid_bounds_value
-};
-
-
-
-
-static jsnn_parser parser;
 static jsnntok_t tokens[JSNN_TOKEN_COUNT];
+static jsnn_parser parser;
 
-
-static int
-token_strcmp(const char *json, jsnntok_t *token, const char *s) {
-    return strncmp(json + token->start, s, token->end - token->start);
-}
-
-
-static int
-parse_value(const jsnntok_t *ntok, const jsnntok_t *vtok,
-    const struct jsnn_attr *attr, void *data)
-{
-    const struct jsnn_attr_arr *attr_arr;
-    const struct jsnn_attr_str *attr_str;
-    size_t i, len;
-    char null_swap;
-
-
-    switch (vtok->type) {
-    case JSNN_ARRAY:
-        attr_arr = (const struct jsnn_attr_arr *)attr;
-        for (i = 0; i < attr_arr->len; i++) {
-            null_swap = json[vtok->end];
-            json[vtok->end] = '\0';
-            (*attr_arr->put)(i, json + vtok->start, data);
-            json[vtok->end] = null_swap;
-        }
-        break;
-    case JSNN_STRING:
-        attr_str = (const struct jsnn_attr_str *)attr;
-        len = vtok->end - vtok->start;
-        if ((len + 1) > attr_str->len) {
-            /* Token string is too long for buffer. */
-            return -1;
-        }
-        strncpy(attr_str->buf, json + vtok->start, len);
-        attr_str->buf[len] = '\0';
-        break;
-    }
-
-}
-
-static int
-parse_request(const char *msg, const struct jsnn_attr **attrs, void *data) {
+int parse_request(const char *msg, gf_grid *grid) {
     jsnnerr_t err;
-    jsnntok_t *ntok, *vtok;
-    const char *name, * const *path_part;
-    const struct jsnn_attr **attrp, *attr;
+    jsnntok_t *bounds, *fmt, *vals, *res;
+    char index_str[10];
+    int pos;
+    /* Grid variables */
+    double val, lat, lng, width, height;
+    int nlat, nlng;
 
     printf("message: %s\n\n", msg);
 
@@ -137,113 +29,62 @@ parse_request(const char *msg, const struct jsnn_attr **attrs, void *data) {
         return -1;
     }
 
-    for (ntok = tokens;; ntok++) {
-        if (ntok->start == ntok->end)
-            break;
-
-        if (ntok->pair_type == JSNN_NAME) {
-            for (attrp = attrs; attrp != NULL; attrp++) {
-                attr = *attrp;
-                for (path_part = attr->path; *path_part != NULL; path_part++) {
-                    if (token_strcmp(msg, ntok, *path_part) != 0)
-                        break;
-                }
-                if (*path_part == NULL) {
-                    /* Path match */
-                    vtok = ntok + 1;
-                    if (vtok->type == attr->type)
-                        parse_value(ntok, vtok, attr, data);
-                }
-            }
-        }
-
-        printf("Token\n  type: %d\n  Position: %d\n  End: %d\n  "
-            "Parent: %d\n  Size: %d\n  Value: %.*s\n",
-            token->type,
-            token->start,
-            token->end,
-            token->parent,
-            token->size,
-            token->end - token->start,
-            msg + token->start);
+    /* Get bounds object */
+    bounds = jsnn_get(tokens, "bounds", msg, tokens);
+    fmt = jsnn_get(bounds, "format", msg, tokens);
+    if (fmt->end - fmt->start != 4) {
+        printf("Wrong number of characters in bounds format string: %d", fmt->end - fmt->start);
+        return -1;
+    }
+    vals = jsnn_get(bounds, "values", msg, tokens);
+    if (vals->size != 4) {
+        printf("Need 4 bounds values. Got %d", vals->size);
+        return -1;
     }
 
+    for (pos = fmt->start; pos < fmt->end; pos++) {
+        sprintf(index_str, "[%d]", pos - fmt->start);
+        val = atof(msg + jsnn_get(vals, index_str, msg, tokens)->start);
+
+        switch (msg[pos]) {
+        case 'N':
+            lat = val;
+            break;
+        case 'W':
+            lng = -val;
+            break;
+        case 'w':
+            width = val;
+            break;
+        case 'h':
+            height = val;
+            break;
+        }
+    }
+
+    /* Get grid resolution */
+    res = jsnn_get(tokens, "resolution", msg, tokens);
+    switch (res->type) {
+    case JSNN_ARRAY:
+        nlng = atoi(msg + jsnn_get(res, "[0]", msg, tokens)->start);
+        nlat = atoi(msg + jsnn_get(res, "[1]", msg, tokens)->start);
+        break;
+    case JSNN_PRIMITIVE:
+        nlng = nlat = atoi(msg + res->start);
+        break;
+    default:
+        printf("Got weird value for grid resolution.\n");
+        return -1;
+    }
+
+    printf("lat = %f\n"
+        "lng = %f\n"
+        "width = %f\n"
+        "height = %f\n"
+        "nlat = %d\n"
+        "nlng = %d\n",
+        lat, lng, width ,height, nlat, nlng);
+    gf_init_grid_point(grid, lat, lng, width, height, nlat, nlng);
     return 0;
 }
 
-
-
-/*
- * Parses bounds info from json message and stores in gridfloat
- * grid struct.
- *
- * @param token Must point to the token in the jsnn array of
- *     tokens that 
- *
- * @returns A pointer to the next json token that should be
- *     parsed. NULL if parsing failed. If NULL is returned,
- *     an error message has been set in parse_err_msg.
- */
-static jsnntok_t *
-parse_attr(const char *json, jsnntok_t *token, gf_grid *grid) {
-    
-
-    int parent, attrs_parsed;
-
-
-    if (token_strcmp(json, token, "bounds") != 0)
-        return NULL;
-
-    parent = token->parent;
-    
-    /* First token after "bounds" attr name token is bounds object.
-       Confirm this. Also confirm that object's parent is same
-       as bounds attr name. */
-    token++;
-    if (token->type != JSNN_OBJECT || token->parent != parent)
-        return -1;
-
-    /* Next token should be "fmt" or "values" attr name. */
-    attrs_parsed = 0;
-    while (attrs_parsed < 2) {
-        token++;
-
-        if (token_strcmp(json, token, "fmt") == 0)
-            token = parse_bounds_fmt(json, token);
-        else if (token_strcmp(json, token, "fmt") == 0)
-            token = parse_bounds_fmt(json, token);
-
-
-        attrs_parsed++;
-
-    }
-
-
-
-}
-
-
-
-/*
- * Put it all together
- */
-struct attr_parser_info parsers[] = {
-    {
-        (const char **)grid_bounds_format_path,
-        JSNN_STRING,
-        attr_parser__grid_bounds_format
-    },
-    { grid_bounds_values_path, JSNN_ARRAY, parse_grid_bounds_values },
-    { grid_resolution_path, JSNN_ARRAY, parse_grid_resolution }
-};
-
-
-static int
-parse_request(const char *json, struct request *req) {
-    struct msg m;
-
-    char grid_bounds_values_path[3][128] = { "grid", "bounds", "values" };
-    char grid_resolution_path[2][128] = { "grid", "resolution" };
-
-
-    parse_json(msg, parsers, &m);
